4bedeb68e6e5b1f81988be450667a201fbce0d9b
jeffdegraef <jeffdegraef@gmail.com>
2017-05-20 09:39:32 +0200

    DevOps setup v1

---
+++ test/test_helper.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Mijn PC on 20/05/2017.
+ */
+
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
+
+chai.use(chaiImmutable);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Mijn PC on 20/05/2017.
+ */
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'babel-polyfill',
+        './src/main'
+    ],
+    output: {
+        publicPath: '/',
+        filename: 'main.js'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, 'src'),
+                loader: 'babel-loader',
+                query: {
+                    presets: ["es2015"],
+                }
+            }
+        ]
+    },
+    debug: true
+};
